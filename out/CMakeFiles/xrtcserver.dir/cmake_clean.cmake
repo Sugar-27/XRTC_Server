3f69869658bf2728fd130ad4227aba6f9e8103e9
Sugar <45682h@gmail.com>
2022-11-24 11:14:05 +0800

    搭建编译框架,使用CMake来完成

---
+++ out/CMakeFiles/xrtcserver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xrtcserver.dir/src/main.cpp.o"
+  "xrtcserver"
+  "xrtcserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xrtcserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
